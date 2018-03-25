bb2cd47f62e9a9ee4aa5482c03b0ccc2d2c59bd3
Nikolay Peychev <33577726+NikolayPeychev@users.noreply.github.com>
2018-03-25 17:04:08 +0300

    Add files via upload

---
+++ Exceptions/ConsoleApplication17/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace Demo1
+    {
+        public class Car
+        {
+            private float fuelLeft;
+            private float fuelPerMile;
+            private float kilometers;
+
+            public Car(float fuelLeft, float fuelPerMile)
+            {
+                if (fuelLeft < 1)
+                {
+                    throw new ArgumentException("Negative Fuel Number! Fuel number must be more then 0");
+                }
+                this.fuelLeft = fuelLeft;
+
+                if (fuelPerMile < 1)
+                {
+                    throw new ArgumentException("Fuel number must be more then 0");
+                }
+                this.fuelPerMile = fuelPerMile;
+            }
+
+            public float Mileage()
+            {
+                kilometers = this.fuelLeft / this.fuelPerMile;
+                return kilometers;
+            }
+        }
+    }
+
---
+++ Exceptions/ConsoleApplication17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Demo1
+{
+    public class Demo1
+    {
+        public static void Main()
+        {
+
+            while (true)
+            {
+                Console.Write("Please, enter the fuel ammount: ");
+                var fuelLeft = float.Parse(Console.ReadLine());
+                Console.Write("Please, enter the fuel per mile: ");
+                var fuelPerMile = float.Parse(Console.ReadLine());
+
+                try
+                {
+                    Car opel = new Car(fuelLeft, fuelPerMile);
+                    Console.WriteLine("You can drive {0} km", opel.Mileage());
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+    }
+}
+
+
